2cb8300a9196f8b5c8f19ccbca56751991155764
Moustafaplusplus <moustafafedo1993@gmail.com>
2025-04-06 09:46:27 +0400

    new updated

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,18 @@ import Logo from './assets/Logo .svg'
 
 function Header(){
     return(
-        <header>
+        <header className="header">
             <img src={Logo} alt="logo" style={{width:'100px'}}/>
+        <nav className="nav">
+            <ul>
+                <li><a href='./home'>Home</a></li>
+                <li><a href='./menu'>Menu</a></li>
+                <li><a href='./about'>About</a></li>
+                <li><a href='./reservations'>Reservations</a></li>
+                <li><a href='./online-orders'>Online Orders</a></li>
+                <li><a href='./login'>Login</a></li>
+            </ul>
+        </nav>
         </header>
     )
 }
--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,5 @@
 
+
 function Main(){
     return(
         <main>
